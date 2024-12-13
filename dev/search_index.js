d2a32070bdd5c6dbc78556ece3e1036045976447
Documenter.jl <documenter@juliadocs.github.io>
2024-12-13 03:55:17 +0000

    build based on 583322f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReadMatlabDocstrings","category":"page"},{"location":"#ReadMatlabDocstrings","page":"Home","title":"ReadMatlabDocstrings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReadMatlabDocstrings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReadMatlabDocstrings]","category":"page"}]
+}
